997bb463ba7aa20256ad06cb1b856b28331e0126
Shohel Rana <cse_shohel@yahoo.com>
2019-09-18 18:34:27 +0600

    added indent, outdent

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
     var markDownEl = document.querySelector(".editor > .right > pre");
     var mdEd = new MediumEditor(".editor > .left", {
         toolbar: {
-            buttons: ['bold', 'italic', 'underline', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'h1', 'h2', 'h3', 'h4', 'unorderedlist', 'orderedlist', 'anchor', 'image', 'removeFormat']
+            buttons: ['bold', 'italic', 'underline', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'h1', 'h2', 'h3', 'h4', 'unorderedlist', 'orderedlist', 'indent', 'outdent', 'anchor', 'image', 'removeFormat']
         },
         extensions: {
             markdown: new MeMarkdown(function (md) {
