4c6fbee936b9b2f1bdf567973eba83bd0d6d73a6
Shohel Rana <cse_shohel@yahoo.com>
2019-09-18 17:00:55 +0600

    add toolbar

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
     var markDownEl = document.querySelector(".editor > .right > pre");
     new MediumEditor(".editor > .left", {
         toolbar: {
-            buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', "unorderedlist"]
+            buttons: ['bold', 'italic', 'underline', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'h1', 'h2', 'h3', 'h4', 'unorderedlist', 'orderedlist', 'anchor', 'image', 'removeFormat']
         },
         extensions: {
             markdown: new MeMarkdown(function (md) {
